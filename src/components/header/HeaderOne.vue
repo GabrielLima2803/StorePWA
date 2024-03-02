660638b8049ca035ba124d8235c8b6a9b4c5322a
GabrielLima2803 <gabriellima2803@gmail.com>
2024-03-01 21:53:24 -0300

    Add Icons in Header

---
+++ src/components/header/HeaderOne.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+        <div class="left">
+            <Menu />
+        </div>
+        <div class="right">
+            <Account />
+        </div>
+    </div>
+</template>
+  
+<script setup>
+import Account from 'vue-material-design-icons/Account.vue';
+import Menu from 'vue-material-design-icons/Menu.vue';
+</script>
+  
+<style scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.left,
+.right {
+    padding: 10px 10px;
+}
+</style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
+import HeaderOne from '@/components/header/HeaderOne.vue';
 </script>
 
 <template>
+  <header-one/>
   <div>
     <h1>Hellow World!!!</h1>
   </div>
